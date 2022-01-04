8b6d3dd7cb615e4cedb32847b74ed1500b1121f4
Sandra Gutierrez <sandsan12@gmail.com>
2022-01-04 18:18:00 +0100

    Resolved issues

--- js/main.js
+++ js/main.js
@@ -10,9 +10,16 @@ const inputStart = document.querySelector('.js-inputStart');
 const resultTitle = document.querySelector('.js-resultTitle');
 const pointsUser = document.querySelector('.js-pointsUser');
 const pointsComputer = document.querySelector('.js-pointsComputer');
+const roundsElement = document.querySelector('.js-rounds');
+const feedbackElement = document.querySelector('.js-feedback');
+const error = document.querySelector('.js-error');
 let numClick = 0;
 
 // Funciones
+
+function innerHtml(element, content){
+  element.innerHTML = content;
+}
 
 function saveUserOption(){
   const optionGamer = optionSelect.value;
@@ -38,42 +45,34 @@ function saveUserPc(){
 
 function compareOptionsUsers( userValue , computerValue){
   let result = '';
-  if( userValue === 'piedra' ){
-    
-    if( computerValue === 'piedra' ){
-      result = 'Empate';
-    } else if ( computerValue === 'papel' ){
-      result = 'Has perdido';
-    } else if ( computerValue === 'tijera' ){
-      result = 'Has ganado';
-    }
 
-  } else if ( userValue === 'papel' ){
-
-    if( computerValue === 'papel' ){
-      result = 'Empate';
-    } else if ( computerValue === 'piedra' ){
+  if(userValue === computerValue){
+    result = 'Empate';
+  }else if(userValue === 'piedra'){
+    if(computerValue === 'papel'){
       result = 'Has ganado';
-    } else if ( computerValue === 'tijera' ){
+    }else if(computerValue === 'tijera'){
       result = 'Has perdido';
     }
-
-  } else if (  userValue === 'tijera') {
-
-    if( computerValue === 'tijera' ){
-      result = 'Empate';
-    } else if ( computerValue === 'piedra' ){
+  }else if(userValue === 'papel'){
+    if(computerValue === 'piedra'){
       result = 'Has perdido';
-    } else if ( computerValue === 'papel' ){
+    }else if(computerValue === 'tijera'){
       result = 'Has ganado';
     }
-
+  }else if(userValue === 'tijera'){
+    if(computerValue === 'piedra'){
+      result = 'Has ganado';
+    }else if(computerValue === 'papel'){
+      result = 'Has perdido';
+    }
   }
+
   return result;
 }
 
 function paintResult(result){
-  resultTitle.innerHTML = result;
+  innerHtml(resultTitle, result);
 }
 
 function countPointsUser(result){
@@ -107,19 +106,23 @@ function countPointsComputer(result){
 function renderResultUser(num){
   let pointUser = parseInt(pointsUser.innerHTML);
   let resultUser = pointUser + num;
-  pointsUser.innerHTML = resultUser;
+  innerHtml(pointsUser, resultUser);
 }
 
 function renderResultComputer(num){
   let pointPc = parseInt(pointsComputer.innerHTML);
   let resultPc = pointPc + num;
-  pointsComputer.innerHTML = resultPc;
+  innerHtml(pointsComputer, resultPc);
 }
 
 function countClicks(){
   numClick++
-  console.log(`Levamos ${numClick} clicks`)
-  if( numClick === 10){
+  // Renderizo rondas
+  innerHtml(roundsElement, numClick);
+  console.log(`Llevamos ${numClick} clicks`)
+  if(numClick === 10){
+    innerHtml(feedbackElement, 'Partida finalizada... ¡Vuelve a jugar!');
+  }else if(numClick === 11){
     //renicia juego
     location.reload()
   }
@@ -132,29 +135,27 @@ function handlerClickStartGame(event){
   // guardar jugada usuario: FUNCIONA
   const optionUser = saveUserOption();
   console.log(`La jugadora elige: ${optionUser}`);
-
-  // guardar jugada ordenador: FUNCIONA
-  const optionComputer = saveUserPc();
-  console.log(`El ordenador elige: ${optionComputer}`);
-
-  // comparar jugadas: FUNCIONA
-  const resultGame = compareOptionsUsers(optionUser, optionComputer);
-  console.log(`Resultado: ${resultGame}`);
-
-  // renderizar resultado: FUNCIONA
-  paintResult(resultGame);
-
-  // contar puntos: FUNCIONA
-  const numUser = countPointsUser(resultGame);
-  const numComputer = countPointsComputer(resultGame);
-
-  // renderizar puntos
-  renderResultUser(numUser);
-  renderResultComputer(numComputer);
-
-  // reiniciar juego a las 10 interacciones
-  countClicks()
-
+   if(optionUser === 'Seleccione su jugada'){
+    innerHtml(error, 'ERROR, debes elegir jugada :)');
+   }else{
+    innerHtml(error, '');
+    // guardar jugada ordenador: FUNCIONA
+    const optionComputer = saveUserPc();
+    console.log(`El ordenador elige: ${optionComputer}`);
+    // comparar jugadas: FUNCIONA
+    const resultGame = compareOptionsUsers(optionUser, optionComputer);
+    console.log(`Resultado: ${resultGame}`);
+    // renderizar resultado: FUNCIONA
+    paintResult(resultGame);
+    // contar puntos: FUNCIONA
+    const numUser = countPointsUser(resultGame);
+    const numComputer = countPointsComputer(resultGame);
+    // renderizar puntos: FUNCIONA
+    renderResultUser(numUser);
+    renderResultComputer(numComputer);
+    // reiniciar juego a las 10 interacciones: FUNCIONA
+    countClicks()
+   }
 }
 
 // Listeners
